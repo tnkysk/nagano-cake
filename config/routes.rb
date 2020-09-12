56555be2210a62533cdae05716e892cff1875a5c
vagrant <vagrant@localhost.localdomain>
2020-09-12 07:51:10 +0000

    Add:管理者ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :admins,
+  path: "admin",
+  controllers:{
+    sessions: 'devise/admins/sessions'
+  }
+
 end
